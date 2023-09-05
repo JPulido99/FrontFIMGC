9e51ff7d3a7a1403ff651bf82712b5b84eecd0aa
JPulido999 <junior.pulido.27@unsch.edu.pe>
2023-09-05 11:22:28 -0500

    UPDATE direccion build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build' // Aquí configura la carpeta de salida para tu construcción
+  }
 })
